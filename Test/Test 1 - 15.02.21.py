ae1ffd7fd539d9ffe918535086f9f4bb242ad923
Petrawantstocode <78495423+Petrawantstocode@users.noreply.github.com>
2021-02-15 17:13:40 +0200

    беда-беда :/

---
+++ Test/Test 1 - 15.02.21.py
@@ -0,0 +1,17 @@
+# 1
+# name = input("Please enter your first name: ")
+# surname = input("Please enter your last name: ")
+# age = input("Please enter your age: ")
+# print("Hello, " + surname +" " + name +". Your age is: " + age)
+
+# # 3
+# side_a = float(input("Please enter length of side 1: "))
+# side_b = float(input("Please enter length of side 2: "))
+# side_c = float(input("Please enter length of side 3: "))
+# if float(side_c ** side_c) == sum(float(side_b ** side_b), float(side_b ** side_b))
+#     print("The triangle is right")
+# else
+#     print("The triangle is not right")
+
+# 4
+client_list = [input("Please enter your list: ")]
